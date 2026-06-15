5759fd467eb49efe23230ea29ed9752f4c16041a
ELAP-sys <amirparsa793@hotmail.com>
2026-06-15 23:45:56 +0330

    update2

---
+++ content/inx.js
@@ -0,0 +1,30 @@
+// Dark mode toggle
+const toggle = document.getElementById("toggle");
+const icon = document.getElementById("mode-icon");
+toggle.addEventListener("click", () => {
+    document.body.classList.toggle("light");
+    if (document.body.classList.contains("light")) {
+        icon.textContent = "☀️";
+    } else {
+        icon.textContent = "🌙";
+    }
+});
+
+const logoDropdown = document.getElementById("logoDropdown");
+const dropdownMenu = document.getElementById("dropdownMenu");
+
+logoDropdown.addEventListener("click" || "hover", (e) => {
+    e.stopPropagation();
+    const isOpen = dropdownMenu.style.display === "flex";
+    dropdownMenu.style.display = isOpen ? "none" : "flex";
+});
+
+document.addEventListener("click", () => {
+    dropdownMenu.style.display = "none";
+});
+
+document.getElementById("scrollBtn").addEventListener("click", () => {
+    document.getElementById("targetSection").scrollIntoView({
+        behavior: "smooth"
+    });
+});
